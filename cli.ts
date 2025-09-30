1f6d01c44e56f68660e0f0753800a1693fe27c16
Abhinav Pandey <abhinavpandey1230@gmail.com>
2025-09-30 16:45:00 +0530

    Add main entry point and CLI interface

---
+++ cli.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env ts-node
+
+/**
+ * CLI Entry Point - Hedge Trading Bot
+ */
+
+import { runCli } from "./src/interfaces/cli/index";
+
+// Run the CLI
+runCli().catch((error) => {
+  console.error("Fatal error:", error);
+  process.exit(1);
+});
